de674925c031d245e5dc60536ac7d2915f639141
roman.aimaletdinov <aymaletdinov2012@gmail.com>
2023-10-28 20:17:14 +0100

    lib versions update

--- code_coverage/build.gradle.kts
+++ code_coverage/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("plugin.serialization") version "1.8.22"
+    kotlin("plugin.serialization") version "1.9.10"
 }
 
 repositories {
@@ -19,9 +19,9 @@ gradlePlugin {
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:8.1.1")
-    implementation(kotlin("gradle-plugin", "1.8.22"))
+    implementation("com.android.tools.build:gradle:8.1.2")
+    implementation(kotlin("gradle-plugin", "1.9.10"))
 
-    implementation(kotlin("serialization", "1.8.22"))
+    implementation(kotlin("serialization", "1.9.10"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
 }
